e9477c98f5af87e684fe928d192c806c01eaa554
Uwituze David <uwituzedavide@gmail.com>
2025-10-17 15:08:01 +0200

    Adding a Homepage to my app

---
+++ DevConnect/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+    return ( <>
+    <div className="pt-50 text-center bg-blue-800 h-screen">
+    <h1 className="text-4xl font-bold mb-4 ">Welcome to DevConnect</h1>
+        <p className="text-lg text-white mb-6">Connecting developers and recruiters to build a smarter planet.</p>
+        <a href="/directory" className="bg-blue-950 text-white px-6 py-2 rounded hover:bg-blue-700">Explore Directory</a>
+    </div>
+    
+    </>
+       
+     );
+}
+
+export default Home;
